505728b311235b4aadc3e4bff805f39dc07314ba
Agustina <agustina.barbetta@gmail.com>
2016-02-24 05:33:40 +0000

    Variable for mongoURI

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var express = require('express'),
     routes = require('./app/routes/index.js'),
     api = require('./app/ant-url.js'),
     mongo = require('mongodb').MongoClient,
+    mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ant-url',
     port = process.env.PORT || 8080,
     app = express();
 
-mongo.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ant-url', function (err, db) {
+mongo.connect(mongoUri, function (err, db) {
   
   if (err) throw new Error('Database failed to connect!');
   else console.log('MongoDB successfully connected on port 27017.');
